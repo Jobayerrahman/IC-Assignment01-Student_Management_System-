44bf35c1b8306ad048801b70dbf1a97a8c7c34a6
jobayerrahman <jobayer.aiub@gmail.com>
2026-01-19 21:24:14 +0600

    Final Commit Of Assignment 01

--- main.py
+++ main.py
@@ -1,32 +1,42 @@
 class Course: 
     FILE = "courses.txt" 
-
-    def __init__(self,course_name,course_id): 
-        self.course_name = course_name 
-        self.course_id = course_id 
         
     @staticmethod 
-    def create_courses(course_id, course_name): 
+    def add_courses(): 
+        course_id = input("Enter course id: ").strip()
+        course_name = input("Enter course name: ").strip()
+
         with open(Course.FILE, "a") as file: 
             file.write(f"{course_id},{course_name} \n") 
+
+        print("Course added successfully!\n")
             
     @staticmethod 
-    def get_all(): 
+    def show_all_courses(): 
         course_list = [] 
-        with open(Course.FILE, "r") as file: 
-            for course in file: 
-                course = course.strip()
-                if not course:
-                    continue
+
+        try:
+            with open(Course.FILE, "r") as file: 
+                for course in file: 
+                    course = course.strip()
+                    if not course:
+                        continue
 
 
-                parts = course.split(",")
-                if len(parts) !=2 :
-                    continue
+                    course_part = course.split(",")
+                    if len(course_part) !=2 :
+                        continue
 
-                course_id, course_name = parts
-                course_list.append((course_id, course_name)) 
+                    course_list.append((course_part[0], course_part[1])) 
+
+        except FileNotFoundError:
+            pass
+
         return course_list
+    
+    @staticmethod
+    def get_all_course_ids():
+        return [course_id for course_id, _ in Course.show_all_courses()]
 
 
 
@@ -46,50 +56,119 @@ class Student:
     
 
     def save(self):
-        courses = "|".join(self.course_list)
+        courses_str = "|".join(self.course_list)
         with open(Student.FILE, "a") as file:
-            file.write(f"{self.student_roll}, {self.student_name},{self.__student_password}, {courses}\n")
+            file.write(f"{self.student_roll}, {self.student_name},{self.__student_password}, {courses_str}\n")
+
 
     @staticmethod
-    def get_all_students():
+    def add_student():
+        student_roll = input("Enter roll: ").strip()
+        student_name = input("Enter name: ").strip()
+        student_password = input("Enter password: ").strip()
+        courses_input = input("Enter course ids (use | between courses, e.g. CSE101|CSE102): ").strip()
+        
+
+        input_courses = courses_input.split("|") if courses_input else []
+        valid_courses = Course.get_all_course_ids()
+        enrolled_courses = []
+        invalid_courses = []
+
+        enrolled_courses = []
+        invalid_courses = []
+
+        for course in input_courses:
+            if course in valid_courses:
+                enrolled_courses.append(course)
+            else:
+                invalid_courses.append(course)
+
+        if invalid_courses:
+            print("This course don't opened evet:", ", ".join(invalid_courses))
+            print("Student Add Failed! \n")
+            return
+        
+
+        student =  Student(
+            student_roll, 
+            student_name, 
+            student_password, 
+            enrolled_courses
+        )
+        student.save()
+
+        print("Student added successfully!\n")
+
+
+    @staticmethod
+    def show_all_students():
         student_list = []
 
-        with open(Student.FILE, "r") as file:
-            for line in file:
-                student_roll, student_name, student_password, courses = line.strip().split(",")
-                course_list = courses.split("|") if courses else []
-                student = Student(student_roll, student_name, student_password, course_list)
-                student_list.append(student)
+        try:
+
+            with open(Student.FILE, "r") as file:
+                for student_row in file:
+                    student_row = student_row.strip()
+
+                    if not student_row:
+                        continue
+                    
+                    student_data_part = student_row.split(",")
+
+                    if len(student_data_part) != 4:
+                        continue
+
+                    student_roll, student_name, student_password, courses = student_data_part
+                    course_list = courses.split("|") if courses else []
+                    student = Student(student_roll, student_name, student_password, course_list)
+                    student_list.append(student)
+        
+        except FileNotFoundError:
+            pass
         return student_list
 
 
-Course.create_courses("course-1001","Python") 
-Course.create_courses("course-1002","SQL") 
-
-s1 = Student(1, "Rahim", "pass123", ["CSE101", "CSE102"])
-s2 = Student(2, "Karim", "abc456", ["CSE101"])
-
-s1.save()
-s2.save()
 
 
-
-print("------ Course Information ------") 
-for course_id, course_name in Course.get_all():
-    print(course_id, course_name)
-print("------ Course End ------") 
-
-
-print("\n")
-
+while True:
+    print("*************< Main Menu >*************")
+    print("1. Add Course")
+    print("2. View All Course")
+    print("3. Add New Courses")
+    print("4. View All Student")
+    print("5. Exit")
 
 
-students = Student.get_all_students()
-print("------ Student Information ------")
-for stu in students:
-    print("Roll:", stu.student_roll)
-    print("Name:", stu.student_name)
-    print("Courses:", stu.course_list)
-    print("Password:", stu.get_password())
+    option = input("Enter your choice: ").strip()
 
-print("------ Student End ------")
+    if option == "1":
+        Course.add_courses()
+
+    elif option == "2":
+        print("\n*************< Course List >*************")
+        print("\n")
+        for CourseId, CourserName in Course.show_all_courses():
+            print(CourseId, "-", CourserName)
+        print("\n")
+
+    elif option == "3":
+        Student.add_student()
+
+    elif option == "4":
+        print("\n*************< Student List >*************")
+        print("\n")
+        for i, stu in enumerate(Student.show_all_students(), start=1):
+            print(
+                f"{i}. Roll: {stu.student_roll} | "
+                f"Name: {stu.student_name} | "
+                f"Courses: {stu.course_list} | "
+                f"Password: {stu.get_password()}"
+            )
+        print("\n")
+
+    elif option == "5":
+        print("Program exited.")
+        break
+
+    else:
+        print("Sorry Invalid choice!\n")
